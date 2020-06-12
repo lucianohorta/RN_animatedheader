70cbc75299c1d48c2b49c41331b73677673eb70a
Luciano Horta <lucianohortasantos@gmail.com>
2020-06-12 00:15:15 -0300

    First commit!

--- App.js
+++ App.js
@@ -1,19 +1,72 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Image, Text, View, SafeAreaView, ScrollView, Animated, StatusBar } from 'react-native';
 
 export default function App() {
+  const [scrollY, setScrollY] = useState(new Animated.Value(0));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView>
+      <StatusBar backgroundColor="red" />
+      <Animated.View 
+        style={[styles.header, 
+        { 
+          height: scrollY.interpolate({ inputRange: [10, 160, 185], outputRange: [110, 20, 0], extrapolate: 'clamp'}),
+          opacity: scrollY.interpolate({ inputRange: [1, 75, 170], outputRange: [1, 1, 0], extrapolate: 'clamp'}) 
+        }
+        ]}
+      >
+        <Animated.Image 
+          source={require('./assets/hamburguer.png')} 
+          style={{height: scrollY.interpolate({inputRange: [0, 120], outputRange: [ 36, 20 ],extrapolate: 'clamp' }), 
+          width: 36}} 
+          resizeMode="contain"
+        />
+        <Animated.Image 
+          source={require('./assets/vegan.png')} 
+          style={{height: scrollY.interpolate({inputRange: [0, 120],outputRange: [ 80, 45 ],extrapolate: 'clamp'}), 
+          width: 230, marginLeft: -50}} 
+          resizeMode="contain" 
+        />
+        {/* RIGHT SIDE ICON: */}
+        <View></View>
+
+      </Animated.View>
+
+      <ScrollView 
+      scrollEventThrottle={16}
+      onScroll={Animated.event([{
+        nativeEvent: {
+          contentOffset: { y: scrollY }
+        },
+      }],
+      { useNativeDriver: false }
+      )}>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+      </ScrollView>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  header: {
+    backgroundColor: '#F2F2E8',
+    // backgroundColor: 'black',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: 15,
+    borderBottomWidth: 2,
+    borderBottomColor: "#FFF",
   },
+  box: {
+    height : 300,
+    backgroundColor: '#f9f9f4',
+    margin: 7,
+    borderRadius: 5,
+  }
 });
